505dec1ab9966989d1d185a20aeb0822b47a3379
jenglim <limjeng@ocdigitalnetwork.com>
2019-06-14 18:07:32 +0800

    role bugfixes

--- src/components/Header/UserBlock.js
+++ src/components/Header/UserBlock.js
@@ -8,7 +8,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 
-import Moment from "moment";
+// import Moment from "moment";
 // import UserAvatar from "Components/User/UserAvatar";
 import { RctCard } from "Components/RctCard";
 
