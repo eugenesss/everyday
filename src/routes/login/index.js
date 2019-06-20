65418e36e28c0c2bbe3c4f70b4787e1c4b5401c2
gjocdn <gianjie@ocdigitalnetwork.com>
2019-06-20 17:30:10 +0800

    added login, forget password

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -9,13 +9,19 @@ import Toolbar from "@material-ui/core/Toolbar";
 import { Link, withRouter, } from "react-router-dom";
 import { Form, FormGroup, Input } from "reactstrap";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+
+
 import QueueAnim from "rc-queue-anim";
 
 // app config
 import AppConfig from "Constants/AppConfig";
 
 // redux action
-import { signInUserWithEmailPassword, userResentEmail} from "Actions";
+import { signInUserWithEmailPassword, userResentEmail, handleRegErrorForm} from "Actions";
+
 
 class Signin extends Component {
 
@@ -35,6 +41,8 @@ class Signin extends Component {
     e.preventDefault();
     if (this.state.emailAddress !== "" && this.state.password !== "") {
       this.props.signInUserWithEmailPassword(this.state, this.props.history);
+    } else {
+      this.props.handleRegErrorForm('Please type your email address and password')
     }
   };
 
@@ -73,8 +81,9 @@ class Signin extends Component {
           <div className="session-inner-wrapper" style={{ marginTop: "5%" }}>
             <div className="container">
               <div className="row row-eq-height">
-                <div className="col-sm-3 col-md-3 col-lg-2" />
-                <div className="col-sm-6 col-md-6 col-lg-8">
+                <div className="col-sm-3 col-md-3 col-lg-3" />
+                <div className="col-sm-4 col-md-6 col-lg-6">
+
                   <div className="session-head mb-30 text-center">
                     <h1 className="mb-20">
                       <Link to="/">
@@ -94,16 +103,17 @@ class Signin extends Component {
                       Most powerful CRM in SG, some say Batam
                     </p>
                   </div>
-                  <div
-                    className="session-body text-center"
-                    style={{ padding: "5% 10%" }}
-                  >
+
+                  <div className="session-body text-center" style={{borderRadius: 15, paddingLeft: '3rem', paddingRight: '3rem', }}>
                     <Form onSubmit={this.onUserLogin}>
+
                       <FormGroup className="has-wrapper">
                         <Input
                           type="email"
                           value={emailAddress}
+                          style={emailAddress? EmailStyle : emptyField}
                           name="emailAddress"
+                          
                           id="emailAddress"
                           className="has-input input-lg"
                           placeholder="Enter Email Address"
@@ -111,73 +121,75 @@ class Signin extends Component {
                             this.setState({ emailAddress: event.target.value })
                           }
                         />
-                        <span className="has-icon">
+                        {/* <span className="has-icon">
                           <i className="ti-email" />
-                        </span>
+                        </span> */}
                       </FormGroup>
                       
                       {error != "LOGIN_FAILED_EMAIL_NOT_VERIFIED" && 
-                        <FormGroup className="has-wrapper">
-                          <Input
-                            value={password}
-                            type="Password"
-                            name="password"
-                            id="password"
-                            className="has-input input-lg"
-                            placeholder="Password"
-                            onChange={event =>
-                              this.setState({ password: event.target.value })
-                            }
-                          />
-                          <span className="has-icon">
-                            <i className="ti-lock" />
-                          </span>
-                        </FormGroup>
+                        <div style={{display:'flex', flexDirection:'column', marginBottom:'6rem'}}>
+                          <FormGroup className="has-wrapper" style={{marginBottom:'0.5rem'}}>
+                            <Input
+                              value={password}
+                              style={password? PasswordStyle : emptyField}
+                              type="Password"
+                              name="password"
+                              id="password"
+                              className="has-input input-lg"
+                              placeholder="Password"
+                              onChange={event =>
+                                this.setState({ password: event.target.value })
+                              }
+                            />
+                            
+                            <span className="has-icon">
+                              <i className="ti-eye" />
+                            </span>
+                          </FormGroup>
+
+                          <div onClick={() => this.routeChange('forget')} style={{textAlign: 'right', color: AppConfig.themeColors.danger, fontSize: 14, fontWeight:'500'}}>Forget Password?</div>
+                        </div>
                       }
 
                       {error != "LOGIN_FAILED_EMAIL_NOT_VERIFIED" && 
                         <FormGroup className="mb-15">
-                          <Button
-                            color="primary"
-                            className="btn-block text-white w-100"
-                            variant="contained"
+                     
+                          <Fab
+                            className="text-white"
                             size="large"
-                            disabled={!emailAddress && !password}
+                            style={{backgroundColor : AppConfig.themeColors.primary, marginBottom: '1.5rem'}}
                             type="submit"
                           >
-                            Sign In
-                          </Button>
+                            <Icon>trending_flat</Icon>
+                          </Fab>
+                          
 
-                          <Button
-                            color="primary"
-                            className="btn-block text-white w-100"
-                            variant="contained"
-                            size="large"
-                            type="submit"
-                            // disabled={!email && !password}
-                            onClick={()=> this.routeChange('register')}
-                          >
-                            Create An Account Here
-                          </Button>
+                          <div onClick={() => this.routeChange('register')} style={{display:'flex', flexDirection:'row', justifyContent:"center", alignItems:'center'}}>
+                            <div style={{color: 'rgba(0,0,0,0.5)', fontWeight: 300, fontSize: 14}}>Not signed up yet?</div>
+                            <div style={{color: "black", fontSize: 14, fontWeight:'500', marginLeft: 5}}>Sign Up</div>
+                          </div>
 
-                          <Button
-                            color="secondary"
-                            className="btn-block text-white w-100"
-                            variant="contained"
-                            size="large"
-                            type="submit"
-                            // disabled={!email && !password}
-                            onClick={()=> this.routeChange('forget')}
-                          >
-                            Forgot Password
-                          </Button>
+           
                         </FormGroup>
                       }
 
                       {error == "LOGIN_FAILED_EMAIL_NOT_VERIFIED" && 
                         <FormGroup className="mb-15">
                           
-                          <Button
+                          <Fab
+                            className="text-white"
+                            size="large"
+                            style={{backgroundColor : AppConfig.themeColors.primary, marginBottom: '1.5rem', marginTop: '2rem'}}
+                            onClick={()=> this.resentVerificationEmail()}
+                          >
+                            <Icon>trending_flat</Icon>
+                          </Fab>
+
+                          <div onClick={() => this.routeChange('register')} style={{display:'flex', flexDirection:'column', justifyContent:"center", alignItems:'center'}}>
+                            <div style={{color: 'rgba(0,0,0,0.5)', fontWeight: 300, fontSize: 14}}>You have signed up with us before</div>
+                            <div style={{color: "black", fontSize: 14, fontWeight:'400', marginLeft: 5}}>Resend verification email</div>
+                          </div>
+                          {/* <Button
                             color="primary"
                             className="btn-block text-white w-100"
                             variant="contained"
@@ -185,7 +197,7 @@ class Signin extends Component {
                             onClick={()=> this.resentVerificationEmail()}
                           >
                             Resend verification email
-                          </Button>
+                          </Button> */}
 
                         </FormGroup>
                       }
@@ -193,7 +205,7 @@ class Signin extends Component {
                     </Form>
                   </div>
                 </div>
-                <div className="col-sm-3 col-md-3 col-lg-2" />
+                <div className="col-sm-3 col-md-3 col-lg-3" />
               </div>
             </div>
           </div>
@@ -217,6 +229,26 @@ export default withRouter(connect(
   mapStateToProps,
   {
     signInUserWithEmailPassword,
-    userResentEmail
+    userResentEmail,
+    handleRegErrorForm
   }
 )(Signin));
+
+
+const emptyField = {
+  borderBottom: '0.3px solid rgba(0,0,0,0.5)', borderRadius: 0, padding: 0,
+  boxShadow: 'none', borderTop: 'none', borderLeft: 'none', borderRight: 'none',
+  fontWeight: '300', fontSize: '16px', color : '#ebedf2',
+}
+
+const PasswordStyle = {
+  borderBottom: '0.3px solid rgba(0,0,0,0.5)', borderRadius: 0, padding: 0,
+  boxShadow: 'none', borderTop: 'none', borderLeft: 'none', borderRight: 'none',
+  fontWeight: '500', fontSize: '18px', color : 'black', letterSpacing: '8px'
+}
+
+const EmailStyle = {
+  borderBottom: '0.3px solid rgba(0,0,0,0.5)', borderRadius: 0, padding: 0,
+  boxShadow: 'none', borderTop: 'none', borderLeft: 'none', borderRight: 'none',
+  fontWeight: '500', fontSize: '18px', color : 'black',
+}
