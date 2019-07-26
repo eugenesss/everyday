53e565e5035990426e5b9d26273723c5b379ecce
Eugene Sim <eugene@ocdigitalnetwork.com>
2019-07-26 10:42:44 +0800

    final edits before beta

--- src/lib/reactifyCss.js
+++ src/lib/reactifyCss.js
@@ -1,8 +1,4 @@
 /*======= Include All App Css =======*/
-// Uer For Quill Editor
-import "react-quill/dist/quill.snow.css";
-// Use For Switch
-import "react-toggle-switch/dist/css/switch.min.css";
 // Use Themify Icon
 import "Assets/themify-icons/themify-icons.css";
 // Use For Full Calender
@@ -11,10 +7,6 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-notifications/lib/notifications.css";
 // calendar
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
-// editor
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-//cropper
-import "cropperjs/dist/cropper.css";
 
 // Custom Style File
 import "Assets/scss/_style.scss";
