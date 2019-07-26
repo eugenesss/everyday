4e5ba49207bc2a25bd34ba409c360c5a1e0d7764
Eugene Sim <eugene@ocdigitalnetwork.com>
2019-07-26 14:32:52 +0800

    api endpoints

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "http://api.everydaycrm.sg/api"
+  //baseURL: "http://localhost:3001/api",
   // timeout: 6000,
-
 });
 
 api.interceptors.request.use(config => {
